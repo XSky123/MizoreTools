e76414329a05f0e23b59012494b51b20da4209fe
XSKY123 <dlxiaotian01@gmail.com>
2019-09-22 22:34:25 +0800

    Beta 1 ffmpeg基础功能完成

--- Mizore工具箱/MainWindow.xaml.cs
+++ Mizore工具箱/MainWindow.xaml.cs
@@ -44,7 +44,7 @@ namespace MizoreTools
             LblAbout.Content = "Mizore工具箱是一款整合各种命令行工具并提供友好GUI的快捷工具。\n" +
                 "初衷是解决shell下无法快捷编辑命令的问题。\n" +
                 "软件在测试阶段，出现问题随时联系 Contact@XSky123.com\n\n" +
-                "By Mizore@VoiceMemories & KSLU & CTM & U2";
+                "By Mizore@Voice Memories";
 
 
 
@@ -77,13 +77,12 @@ namespace MizoreTools
         private void ReadConfig()
         {
             /* Youtube-DL */
-            TxtInAddr.Text = ConfigurationManager.AppSettings["U2B_InAddr"].ToString();
-            TxtOutAddr.Text = ConfigurationManager.AppSettings["U2B_OutAddr"].ToString();
 
             ChkProxy.IsChecked = bool.Parse(ConfigurationManager.AppSettings["U2B_EnableProxy"].ToString());
             CmbProxy.SelectedItem = ConfigurationManager.AppSettings["U2B_ProxyType"].ToString();
-            TxtProxyAddr.Text = ConfigurationManager.AppSettings["U2B_ProxyAddr"].ToString();
-            TxtProxyPort.Text = ConfigurationManager.AppSettings["U2B_ProxyPort"].ToString();
+
+            TxtInAddr.Text = ConfigurationManager.AppSettings["U2B_InAddr"].ToString();
+            TxtOutAddr.Text = ConfigurationManager.AppSettings["U2B_OutAddr"].ToString();
 
             ChkAllSub.IsChecked = bool.Parse(ConfigurationManager.AppSettings["U2B_ChkAllSub"].ToString());
             ChkAllAutoSub.IsChecked = bool.Parse(ConfigurationManager.AppSettings["U2B_ChkAllAutoSub"].ToString());
@@ -246,6 +245,246 @@ namespace MizoreTools
         }
 
         /*
+         * ffmpeg 控件部分
+         */
+        private void BtnFFMpegBrowseIn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TxtFFMpegIn.Text = openFileDialog.FileName;
+                TxtFFMpegOut.Text = String.Format("{0}_Mizore{1}",
+                    System.IO.Path.GetFileNameWithoutExtension(openFileDialog.FileName),
+                    System.IO.Path.GetExtension(openFileDialog.FileName));
+            }
+        }
+
+        private void BtnGetWAV_Click(object sender, RoutedEventArgs e)
+        {
+            if(TxtFFMpegIn.Text == "")
+            {
+                System.Windows.Forms.MessageBox.Show("请选择输入文件", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Console.WriteLine(wav_builder());
+            CheckAndRun("ffmpeg", wav_builder(), false);
+        }
+
+        private void BtnGet320KMP4_Click(object sender, RoutedEventArgs e)
+        {
+            if (TxtFFMpegIn.Text == "")
+            {
+                System.Windows.Forms.MessageBox.Show("请选择输入文件", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Console.WriteLine(_320KMP4_builder());
+            CheckAndRun("ffmpeg", _320KMP4_builder(), false);
+        }
+
+        private void BtnGetCopyMP4_Click(object sender, RoutedEventArgs e)
+        {
+            if (TxtFFMpegIn.Text == "")
+            {
+                System.Windows.Forms.MessageBox.Show("请选择输入文件", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            bool need_cut = (TimeStrToSecond(TxtCutEndPos.Text) == TimeStrToSecond(TxtCutStartPos.Text));
+            //Console.WriteLine(copyMP4_builder(need_cut));
+            CheckAndRun("ffmpeg", copyMP4_builder(need_cut), false);
+        }
+
+        private void BtnGetCut_Click(object sender, RoutedEventArgs e)
+        {
+            if (TxtFFMpegIn.Text == "")
+            {
+                System.Windows.Forms.MessageBox.Show("请选择输入文件", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if(TimeStrToSecond(TxtCutEndPos.Text) == TimeStrToSecond(TxtCutStartPos.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("请输入剪辑时间", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Console.WriteLine(cut_builder());
+            CheckAndRun("ffmpeg", cut_builder(), false);
+
+        }
+
+        private void BtnStartAdd1H(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutStartPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutStartPos.Text = "01:00:00";
+            else
+            {
+                timeGroup[0]++;
+                TxtCutStartPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeStart();
+        }
+
+        private void BtnStartAdd1M(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutStartPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutStartPos.Text = "00:01:00";
+            else
+            {
+                timeGroup[1] = (timeGroup[1] + 1) % 60;
+                TxtCutStartPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeStart();
+        }
+
+        private void BtnStartAdd1S(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutStartPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutStartPos.Text = "00:00:01";
+            else
+            {
+                timeGroup[2] = (timeGroup[2] + 1) % 60;
+                TxtCutStartPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeStart();
+        }
+
+        private void BtnStartSub1H(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutStartPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutStartPos.Text = "00:00:00";
+            else
+            {
+                if (timeGroup[0] - 1 < 0)
+                    timeGroup[0] = 48;
+                else
+                    timeGroup[0]--;
+                TxtCutStartPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeStart();
+        }
+
+        private void BtnStartSub1M(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutStartPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutStartPos.Text = "00:00:00";
+            else
+            {
+                if (timeGroup[1] - 1 < 0)
+                    timeGroup[1] = 59;
+                else
+                    timeGroup[1]--;
+                TxtCutStartPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeStart();
+        }
+
+        private void BtnStartSub1S(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutStartPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutStartPos.Text = "00:00:00";
+            else
+            {
+                if (timeGroup[2] - 1 < 0)
+                    timeGroup[2] = 59;
+                else
+                    timeGroup[2]--;
+                TxtCutStartPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeStart();
+        }
+        private void BtnEndAdd1H(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutEndPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutEndPos.Text = "01:00:00";
+            else
+            {
+                timeGroup[0]++;
+                TxtCutEndPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeEnd();
+        }
+
+        private void BtnEndAdd1M(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutEndPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutEndPos.Text = "00:01:00";
+            else
+            {
+                timeGroup[1] = (timeGroup[1] + 1) % 60;
+                TxtCutEndPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeEnd();
+        }
+
+        private void BtnEndAdd1S(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutEndPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutEndPos.Text = "00:00:01";
+            else
+            {
+                timeGroup[2] = (timeGroup[2] + 1) % 60;
+                TxtCutEndPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeEnd();
+        }
+
+        private void BtnEndSub1H(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutEndPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutEndPos.Text = "00:00:00";
+            else
+            {
+                if (timeGroup[0] - 1 < 0)
+                    timeGroup[0] = 48;
+                else
+                    timeGroup[0]--;
+                TxtCutEndPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeEnd();
+        }
+
+        private void BtnEndSub1M(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutEndPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutEndPos.Text = "00:00:00";
+            else
+            {
+                if (timeGroup[1] - 1 < 0)
+                    timeGroup[1] = 59;
+                else
+                    timeGroup[1]--;
+                TxtCutEndPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeEnd();
+        }
+        private void BtnEndSub1S(object sender, RoutedEventArgs e)
+        {
+            int[] timeGroup = ParseTime(TxtCutEndPos.Text);
+            if (timeGroup[0] == -1) // error occurred
+                TxtCutEndPos.Text = "00:00:00";
+            else
+            {
+                if (timeGroup[2] - 1 < 0)
+                    timeGroup[2] = 59;
+                else
+                    timeGroup[2]--;
+                TxtCutEndPos.Text = TimeGroupToStr(timeGroup);
+            }
+            TimeGeneralWhenChangeEnd();
+        }
+
+
+        /*
          * 
          * Youtube-DL 参数控制
          * 
@@ -333,7 +572,98 @@ namespace MizoreTools
             }
         }
 
-       
 
+        /*
+         * 
+         * ffmpeg 参数控制
+         * 
+         */
+        private string wav_builder()
+        {
+            string output = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(TxtFFMpegIn.Text),
+                System.IO.Path.GetFileNameWithoutExtension(TxtFFMpegOut.Text) + ".wav");
+            return String.Format(" -i \"{0}\" \"{1}\"", TxtFFMpegIn.Text, output);
+        }
+
+        private string _320KMP4_builder()
+        {
+            string output = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(TxtFFMpegIn.Text),
+                System.IO.Path.GetFileNameWithoutExtension(TxtFFMpegOut.Text) + ".mp4");
+            return String.Format(" -i \"{0}\" -vcodec copy -acodec aac -b:a 320k \"{1}\"", TxtFFMpegIn.Text, output);
+        }
+
+
+        private string copyMP4_builder(bool need_cut)
+        {
+            string output = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(TxtFFMpegIn.Text),
+                System.IO.Path.GetFileNameWithoutExtension(TxtFFMpegOut.Text) + ".mp4");
+            if (need_cut)
+            {
+                return String.Format(" -i \"{0}\" -ss {1} -t {2} -c copy \"{3}\"", TxtFFMpegIn.Text, TxtCutStartPos.Text, 
+                                                                                   getDiffTimeStr() ,output);
+            }
+            else {
+                return String.Format(" -i \"{0}\" -c copy \"{1}\"", TxtFFMpegIn.Text, output);
+            }
+        }
+        private string cut_builder()
+        {
+            string output = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(TxtFFMpegIn.Text),
+                TxtFFMpegOut.Text);
+
+            return String.Format(" -i \"{0}\" -ss {1} -t {2} -c copy \"{3}\"", TxtFFMpegIn.Text, TxtCutStartPos.Text,
+                                                                               getDiffTimeStr(), output);
+        }
+        /*
+         * 
+         * ffmpeg 剪切时间处理
+         * 
+         */
+        int TimeStrToSecond(string TimeStr)
+        {
+            string[] timeGroup = TimeStr.Split(':');
+            if (timeGroup.Length != 3)
+            {
+                return -1;
+
+            }
+            else
+                return int.Parse(timeGroup[0]) * 3600 + int.Parse(timeGroup[1]) * 60 + int.Parse(timeGroup[2]);
+        }
+
+        int[] ParseTime(string TimeStr)
+        {
+            string[] timeGroup = TimeStr.Split(':');
+            if (timeGroup.Length != 3)
+            {
+                return new int[] { -1, -1, -1};
+
+            }
+            else
+                return new int[] { int.Parse(timeGroup[0]), int.Parse(timeGroup[1]), int.Parse(timeGroup[2]) };
+        }
+        private string TimeGroupToStr(int[] TimeGroup)
+        {
+            return String.Format("{0:D2}:{1:D2}:{2:D2}", TimeGroup[0], TimeGroup[1], TimeGroup[2]);
+        }
+       
+        private void TimeGeneralWhenChangeStart()
+        {
+            if (TimeStrToSecond(TxtCutStartPos.Text) > TimeStrToSecond(TxtCutEndPos.Text))
+                TxtCutEndPos.Text = TxtCutStartPos.Text;
+        }
+        private void TimeGeneralWhenChangeEnd()
+        {
+            if (TimeStrToSecond(TxtCutEndPos.Text) < TimeStrToSecond(TxtCutStartPos.Text))
+                TxtCutStartPos.Text = TxtCutEndPos.Text;
+        }
+       private string getDiffTimeStr()
+        {
+            int time_diff = TimeStrToSecond(TxtCutEndPos.Text) - TimeStrToSecond(TxtCutStartPos.Text);
+            int hour_diff = time_diff / 3600;
+            int min_diff = (time_diff - hour_diff * 3600) / 60;
+            int sec_diff = time_diff - hour_diff * 3600 - min_diff * 60;
+            return String.Format("{0:D2}:{1:D2}:{2:D2}", hour_diff, min_diff, sec_diff);
+        }
     }
 }
